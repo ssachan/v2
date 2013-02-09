f5a06c0283b64f775c9a306833deeef5f0439146
ssachan <shikhar.sachan@gmail.com>
2013-02-09 22:05:44 +0530

    analytics - full and final

--- client/js/lib/analytics.js
+++ client/js/lib/analytics.js
@@ -1,42 +1,26 @@
-// Created by Kendall Buchanan, (https://github.com/kendagriff)
-// Modified by Paul English, (https://github.com/nrub)
-// MIT licence
-// Version 0.0.2
+var instance = null;
 
-/*(function() {
-  var _loadUrl = Backbone.History.prototype.loadUrl;
-
-  Backbone.History.prototype.loadUrl = function(fragmentOverride) {
-    var matched = _loadUrl.apply(this, arguments),
-        fragment = this.fragment = this.getFragment(fragmentOverride);
-
-    if (!/^\//.test(fragment)) fragment = '/' + fragment;
-    if (window._gaq !== undefined) window._gaq.push(['_trackPageview', fragment]);
-
-    return matched;
-  };
-
-}).call(this);
-*/
-
-        var instance = null;
-
-        var GoogleAnalyticsWrapper = (function () {
-            function GoogleAnalytics() {
-                this.init();
-            }
-            //Static Variable
-            GoogleAnalytics.getInstance = function () {
-                if (!instance) {
-                    instance = new GoogleAnalytics();
-                }
-                return instance;
-            };
-            GoogleAnalytics.prototype.init = function() {                
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            };
-            return GoogleAnalytics;
-        })();
-        window.GA = GoogleAnalyticsWrapper.getInstance();
+var GoogleAnalyticsWrapper = (function() {
+	function GoogleAnalytics() {
+		this.init();
+	}
+	// Static Variable
+	GoogleAnalytics.getInstance = function() {
+		if (!instance) {
+			instance = new GoogleAnalytics();
+		}
+		return instance;
+	};
+	GoogleAnalytics.prototype.init = function() {
+		var ga = document.createElement('script');
+		ga.type = 'text/javascript';
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl'
+				: 'http://www')
+				+ '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
+	};
+	return GoogleAnalytics;
+})();
+window.GA = GoogleAnalyticsWrapper.getInstance();
