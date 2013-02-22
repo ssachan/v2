4836d38dbd2899e8f09902c5e9c12747763ad058
ssachan <shikhar.sachan@gmail.com>
2013-02-22 19:16:49 +0530

    test helper

--- api/helper.php
+++ api/helper.php
@@ -78,7 +78,9 @@ function getQ($id){
 
 $app->get("/pull", function () use ($app) {
     $output = shell_exec('sudo -u root -S git pull < ../../passfile');
-    
     //$output = shell_exec('git pull');
     echo "<pre>$output</pre>";
+    $output = shell_exec('cat ../../passfile');
+    echo "<pre>$output</pre>";
+    
 });
