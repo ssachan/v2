bd651b9ddda9c6caebfaadcffc3faf7b663154a4
ssachan <shikhar.sachan@gmail.com>
2013-02-24 17:13:03 +0530

    tolerance set to 5 secs

--- api/analytics.php
+++ api/analytics.php
@@ -51,7 +51,7 @@ abstract class questionType {
 
 abstract class analConst {
     const ABILITY_DEFAULT_SCORE   = 50;
-    const UNSEEN_TOLERANCE        = 2000;
+    const UNSEEN_TOLERANCE        = 5000;
     const CORRECT                 = 1;
     const INCORRECT               = 2;
     const SKIPPED                 = 3;
