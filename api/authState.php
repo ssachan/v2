071a8afce1a1c7d25dbb910526983c7deb550226
ssachan <shikhar.sachan@gmail.com>
2013-03-18 14:41:23 +0530

    forgot password

--- api/authState.php
+++ api/authState.php
@@ -522,9 +522,59 @@ function updatePassword($password, $accountId) {
     doSQL($sqlArray, false);
 }
 
+function generatePassword ($length = 8)
+{
+    // start with a blank password
+    $password = "";
+    // define possible characters - any character in this string can be
+    // picked for use in the password, so if you want to put vowels back in
+    // or add special characters such as exclamation marks, this is where
+    // you should do it
+    $possible = "2346789bcdfghjkmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ";
+    // we refer to the length of $possible a few times, so let's grab it now
+    $maxlength = strlen($possible);
+    // check for length overflow and truncate if necessary
+    if ($length > $maxlength) {
+        $length = $maxlength;
+    }
+    // set up a counter for how many characters are in the password so far
+    $i = 0;
+    // add random characters to $password until $length is reached
+    while ($i < $length) {
+        // pick a random character from the possible ones
+        $char = substr($possible, mt_rand(0, $maxlength-1), 1);
+        // have we already used this character in $password?
+        if (!strstr($password, $char)) {
+            // no, so it's OK to add it onto the end of whatever we've already got...
+            $password .= $char;
+            // ... and increase the counter by one
+            $i++;
+        }
+    }
+    // done!
+    return $password;
+}
+
 $app->post("/forgotpass", $authenticate($app), function () use ($app) {
+    $response = array();
+    if (!(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))) {
+        $response["status"] = FAIL;
+        $response["data"] = "Please give appropriate permission to access your email";
+        break;
+    }
     // generate a new password
-    
+    $email = $_POST['email'];
+    $pass = generatePassword();
+    $account = getAccountByEmail($email);
+    if($account){
+        updatePassword($pass, $accountId);
+        sendMail($email, 'Password Successfully Changed', 'Your new password is '.$pass);
+        $response["status"] = SUCCESS;
+        $response["data"] = "Password successfully changed! Check your email.";
+    }else{
+        $response["status"] = FAIL;
+        $response["data"] = "Account with this email doesn't exist. Please sign-up!";
+    }
     
 });
 
@@ -541,11 +591,13 @@ $app->post("/changepass", $authenticate($app), function () use ($app) {
         updatePassword($newpassword, $accountId);
         $response["status"] = SUCCESS;
         $response["data"] = "Password successfully changed!";
+        //sendMail($email, SIGN_UP_SUB, SIGN_UP_MSG);
     } else {
         //oldpassword doesn't match
         $response["status"] = FAIL;
         $response["data"] = "Old Passwords don't match, try again!";
     }
+    
     sendResponse($response);
 });
 
