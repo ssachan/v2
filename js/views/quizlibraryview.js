cb3776a208c22ec02bc92b9a5e0f627b4220cc55
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2013-03-25 03:07:10 +0530

    Revert "trying to make it smaller"
    This reverts commit bbbbc90afea75ffe2a6cb6f12170578695e547a8.

--- js/views/quizlibraryview.js
+++ js/views/quizlibraryview.js
@@ -104,7 +104,6 @@ window.QuizLibraryView = Backbone.View.extend({
 					new QuizItemView({
 						model : quizzes[i]
 					}).render().el);
-					$(".lol:last").append("<div class='span1'>&nbsp;</div>");
 					i++;
 				}
 				$('.lol').removeClass('lol');
@@ -119,7 +118,7 @@ window.QuizItemView = Backbone.View.extend({
 
 	tagName : "li",
 
-	className : "span3",
+	className : "span4",
 
 	initialize : function() {
 		this.render();
--- js/views/views.js
+++ js/views/views.js
@@ -1313,7 +1313,6 @@ window.QuizLibraryView = Backbone.View.extend({
 					new QuizItemView({
 						model : quizzes[i]
 					}).render().el);
-					$(".lol:last").append("<div class='span1'>&nbsp;</div>");
 					i++;
 				}
 				$('.lol').removeClass('lol');
@@ -1328,7 +1327,7 @@ window.QuizItemView = Backbone.View.extend({
 
 	tagName : "li",
 
-	className : "span3",
+	className : "span4",
 
 	initialize : function() {
 		this.render();
